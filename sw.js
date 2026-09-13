68d0d8d5b6b7557c83641b725cf6ce92c2df2821
mago-del-caos <profesorxjuvenal@gmail.com>
2026-09-12 23:48:13 -0600

    Actualizar sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 // ==========================================
-// SERVICE WORKER - CACHÉ V10 (ESTRUCTURA BLINDADA)
+// SERVICE WORKER - CACHÉ V11 (PURGA TOTAL)
 // ==========================================
 const CACHE_NAME = 'gametria32-v11';
 
 // El catálogo de archivos que conforman el templo, sellados con la versión 11
 const urlsToCache = [
-    './?v=10',
+    './?v=11',
     './index.html?v=11',
     './style.css?v=11',
     './motor.js?v=11',
@@ -20,19 +20,19 @@ self.addEventListener('install', event => {
     self.skipWaiting();
     event.waitUntil(
         caches.open(CACHE_NAME).then(cache => {
-            console.log('Mesa de Trazado blindada guardada en caché (v10).');
+            console.log('Mesa de Trazado purificada guardada en caché (v11).');
             return cache.addAll(urlsToCache);
         })
     );
 });
 
-// Fase de Activación: Purgar las memorias pasadas
+// Fase de Activación: Destruir las memorias pasadas
 self.addEventListener('activate', event => {
     event.waitUntil(
         caches.keys().then(cacheNames => {
             return Promise.all(
                 cacheNames.map(cacheName => {
-                    // Si la caché no es la v10, se elimina del registro
+                    // Si la caché no es la v11, se elimina del registro sin piedad
                     if (cacheName !== CACHE_NAME) {
                         console.log('Borrando caché antigua de Gematría:', cacheName);
                         return caches.delete(cacheName);
